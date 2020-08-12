6500a2b0c400eca0bc1b28f2cd61fa93ffc2d83d
Alana Bacco <alana_bacco@hotmail.com>
2020-08-11 21:40:08 -0300

    Projeto

---
+++ src/pages/Pagina404/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PageDefault from '../../components/PageDefault';
+
+function Pagina404() {
+  return (
+    <PageDefault>
+      <div style={{ textAlign: 'center' }}>
+        <h1>404 - Página não encontrada</h1>
+        <iframe title="Jogo da Hipstar" style={{ width: '80vw', height: '80vh' }} src="https://editor.p5js.org/alanabacco/embed/giJNx5K3w" />
+        <p>Jogo criado na #ImersãoGameDev #Alura</p>
+      </div>
+    </PageDefault>
+  );
+}
+
+export default Pagina404;
