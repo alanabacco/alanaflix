7f5894ce1ddae642e96ccf12b70bb6a8d25aa1f7
Alana Bacco <alana_bacco@hotmail.com>
2022-10-23 16:33:00 -0300

    muda nome da variável URL_BACKEND

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
-const URL_BACKEND_TOP = window.location.hostname.includes('localhost')
-  ? 'http://localhost:8080'
-  : 'https://alanaflix.herokuapp.com';
+const URL_BACKEND = window.location.hostname.includes("localhost")
+  ? "http://localhost:8080"
+  : "https://alanaflix.herokuapp.com";
 
 export default {
-  URL_BACKEND_TOP,
+  URL_BACKEND,
 };
